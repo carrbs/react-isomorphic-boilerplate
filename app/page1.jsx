0245e2941bd35958da5cfd4754dfa19b596d09c7
Ben Carr <carrbens@gmail.com>
2015-03-25 22:02:36 -0700

    added AudioContext component

--- app/page1.jsx
+++ app/page1.jsx
@@ -21,10 +21,48 @@ var Page1 = React.createClass({
                 <h2>Page 1</h2>
                 <p>Welcome to the first page of this website, and, obviously, the best.</p>
                 <p>It's been {this.state.counter} seconds since this page loaded.</p>
+                <Oscillator />
             </div>
         );
     }
 });
 
+var Oscillator = React.createClass({
 
+    getInitialState () {
+        return {oscillator: ''}
+    },
+    componentDidMount () {
+        // create web audio api context
+        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        // create Oscillator node
+        var oscillator = audioCtx.createOscillator();
+        var gainNode = audioCtx.createGain();
+        gainNode.gain.value = 0.010;        
+
+        oscillator.type = 'square';
+        oscillator.frequency.value = 3000; // value in hertz
+
+        oscillator.connect(gainNode);
+        gainNode.connect(audioCtx.destination);
+
+        this.state.oscillator = oscillator;
+    },
+    start () {
+        this.state.oscillator.start();     
+    },
+    stop () {
+        this.state.oscillator.stop(); 
+    },
+
+    render () {
+        return (
+            <div className="oscillatorButtons">
+                <button onClick={this.start}>Start</button>
+                <button onClick={this.stop}>Stop</button>
+            </div>
+        );
+    }
+});
 export default Page1;
+
--- app/welcome.jsx
+++ app/welcome.jsx
@@ -5,8 +5,19 @@ class Welcome {
     render() {
         return (
             <div>
-                <h2>Welcome to the application.</h2>
-                <p>This advanced application will show you 3 different pages, and actually let you click between them. With your web browser.</p>
+                <h2>he came dancing across the water</h2>
+                <p>with his gallions and guns 
+                   looking for the new world and the palace in the sun 
+                   on the shore lay montezuma with his coca leaves and pearls 
+                   in his halls he often wandered the secrets of the world 
+                   and and his subjects gathered round him 
+                   like the leaves around the trees 
+                   in their clothes of many colors for the angry gods to see 
+                   and the women all were beautiful 
+                   and the men stood straight and tall
+                   he offered life and sacrifice 
+                   so that others could go on...
+                </p>
             </div>
         );
     }
