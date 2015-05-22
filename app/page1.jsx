3118f9d9702ca4e0642ef8c2fccb5f8afb47dff7
Ben Carr <carrbens@gmail.com>
2015-05-21 23:34:02 -0700

    add PeriodicWave component, improves starting/stopping functionality, but still doing it wrong

--- app/page1.jsx
+++ app/page1.jsx
@@ -15,13 +15,18 @@ var Page1 = React.createClass({
         delete this._timer;
     },
 
+
     render: function() {
         return (
             <div>
                 <h2>Page 1</h2>
                 <p>Welcome to the first page of this website, and, obviously, the best.</p>
                 <p>It's been {this.state.counter} seconds since this page loaded.</p>
-                <Oscillator />
+                <h2>Oscillator</h2>
+                <p>This is how not to make an oscillator. Enjoy!
+                    <Oscillator />
+                </p>
+                <PeriodicWave />
             </div>
         );
     }
@@ -64,5 +69,53 @@ var Oscillator = React.createClass({
         );
     }
 });
-export default Page1;
 
+var PeriodicWave = React.createClass({
+    getInitialState () {
+        return {oscillator: ''}
+    },
+
+    start() {
+
+        var real = new Float32Array(2);
+        var imag = new Float32Array(2);
+        var ac = new AudioContext();
+        var osc = ac.createOscillator();
+        this.state.oscillator = osc
+
+        real[0] = 1;
+        imag[0] = 0;
+        real[1] = 1;
+        imag[1] = 0;
+
+        var wave = ac.createPeriodicWave(real, imag);
+
+        osc.setPeriodicWave(wave);
+
+        osc.connect(ac.destination);
+
+        osc.start();
+    },
+
+    stop () {
+        this.state.oscillator.stop();
+    },
+
+    increase () {
+        this.state.oscillator.frequency.value = 3000
+    },
+
+    render () {
+        return <div>
+            <h2>Periodic Wave {this.state.oscillator && <span>(Hz: {this.state.oscillator.frequency.value})</span>}</h2>
+            <p>This might be how to make a periodic wave. Enjoy!
+                <div className="wavyButtons">
+                    <button onClick={this.start}>Start</button>
+                    <button onClick={this.stop}>Stop</button>
+                    <button onClick={this.increase}>Increase</button>
+                </div></p>
+        </div>
+    }
+});
+
+export default Page1;
